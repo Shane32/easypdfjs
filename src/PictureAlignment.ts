aa78cbe7f7d699b361385d01ac9db89222dd4a99
Shane Krueger <shane@acdmail.com>
2025-04-23 11:47:20 -0400

    Add paintPicture and pictureAlignment (#12)

---
+++ src/PictureAlignment.ts
@@ -0,0 +1,41 @@
+/**
+ * Specifies a picture alignment when drawing pictures.
+ */
+export enum PictureAlignment {
+  /**
+   * Left-top alignment.
+   */
+  LeftTop,
+  /**
+   * Left-center alignment.
+   */
+  LeftCenter,
+  /**
+   * Left-bottom alignment.
+   */
+  LeftBottom,
+  /**
+   * Center-top alignment.
+   */
+  CenterTop,
+  /**
+   * Center-center alignment.
+   */
+  CenterCenter,
+  /**
+   * Center-bottom alignment.
+   */
+  CenterBottom,
+  /**
+   * Right-top alignment.
+   */
+  RightTop,
+  /**
+   * Right-center alignment.
+   */
+  RightCenter,
+  /**
+   * Right-bottom alignment.
+   */
+  RightBottom,
+}
